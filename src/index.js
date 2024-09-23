51ab575b96418f4eb17bbc26850e611a441b6ec3
Ilya <iosdsbb@gmail.com>
2024-09-23 15:01:36 +0300

    chore: learn buffer Uint8Array

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-const a = new Uint8Array([1,2,3,4]);
+const a1 = new Uint8Array([1,2,3,4]);
 
-console.log(a);
-console.log(a.buffer);
+console.log(a1);
+console.log([...a1]);
+console.log(a1.buffer);
+
+const a2 = new Uint8Array(a1.buffer, 2, 1);
+
+console.log(a2);
+console.log([...a2]);
+console.log(a2.buffer);
