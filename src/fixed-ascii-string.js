4c54cda19dc62c7f5277de6cfe46fb3d46209b6a
Ilya <iosdsbb@gmail.com>
2024-09-24 19:24:39 +0300

    feat: updated FixedAsciiString

--- src/fixed-ascii-string.js
+++ src/fixed-ascii-string.js
@@ -1,5 +1,35 @@
 export const FixedAsciiString = (maxLength) => {
 	return {
+		get byteLength() {
+			return 1;
+		},
 
+		init(buffer, offset) {
+			const
+				arr = new Uint8Array(buffer, offset);
+
+			return {
+				get() {
+					let str = "";
+
+					for (const chardCode of arr) {
+						console.log(chardCode);
+						if (chardCode === 0) {
+							break;
+						}
+
+						str += String.fromCharCode(chardCode);
+					}
+
+					return str;
+				},
+
+				set(str) {
+					for (let i = 0; i< maxLength; i++) {
+						arr[i] = i >= str.length ? 0 : str.charCodeAt(i);
+					}
+				}
+			}
+		}
 	};
 };
